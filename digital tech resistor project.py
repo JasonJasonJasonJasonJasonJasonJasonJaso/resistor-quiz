8ee1f612d70b048061a5e557f129edb87f3024c8
Zexin_wu <zexin_wu@paraparaumucollege.school.nz>
2025-03-24 11:05:22 +1300

    second

--- digital tech resistor project.py
+++
@@ -1,4 +0,0 @@
-print("Hello students, this program will make you answer questions based on the formula \n"
-		  "R = (Vs - Vf) / If")
-
-input("What is the resistance?")
--- testing.py
+++ testing.py
@@ -6,8 +6,21 @@ class quiz:
 		self.current = current
 		self.resistance = 0
 
+	def read_file(self):
+		with open("text.txt", "r") as file:
+			data = file.read().splitlines()
+		led_values = []
+		for i in range(0, len(data), 2):
+			vf = float(data[i])
+			vs = float(data[i + 1])
+			led_values.append((vf, vs))
+
+		return led_values
+
+
 	def calculate_resistor(self):
 		current_amps = self.current / 1000
+		self.resistance = round((self.supply - vf) / current_amps, 2)
 
 	def answers(self):
 		user_answer = round(float(input("What is the resistance?")))
@@ -15,11 +28,6 @@ class quiz:
 			print("Correct!")
 		else:
 			print("Incorrect!")
-
-	def read_file(self):
-		with open("text.txt", "r") as file:
-			data = file.read().splitlines()
-		return data
 
 
 
@@ -29,4 +37,4 @@ def intro():
 		  "R = (Vs - Vf) / If")
 
 intro()
-print("hello worl")
+
