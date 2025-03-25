9492045ac8cb4b4ad0d2d1d82b5a81436e57c827
Zexin_wu <zexin_wu@paraparaumucollege.school.nz>
2025-03-25 13:36:01 +1300

    foruth

--- testing.py
+++ testing.py
@@ -1,28 +1,32 @@
 #This program is to help year 11/10s students in their pythics studies.
 class quiz:
-	def __init__(self, voltage, supply, current, resistance ):
-		self.voltage = voltage
-		self.supply = supply
-		self.current = current
+	def __init__(self):
+		self.voltage = 5
+		self.led_data = self.read_file()
 		self.resistance = 0
 
 	def read_file(self):
 		with open("text.txt", "r") as file:
+
 			data = file.read().splitlines()
+			print(data)
 		led_values = []
 		for i in range(0, len(data), 2):
 			vf = float(data[i])
-			vs = float(data[i + 1])
-			led_values.append((vf, vs))
+			self.icurrent_f = float(data[i + 1])
+			led_values.append((vf, self.icurrent_f))
 
 		return led_values
 
-
 	def calculate_resistor(self):
 		current_amps = self.current / 1000
-		self.resistance = round((self.supply - vf) / current_amps, 2)
+		self.resistance = round((self.voltage - vf) / current_amps, 2)
+		return self.resistance
 
 	def answers(self):
+		correct_resistance = self.calculate_resistor(vf, current)
+		print(f"vf = {vf}v and If = {current}mA")
+		print(f"The correct resistance is {correct_resistance} ohms.")
 		user_answer = round(float(input("What is the resistance?")))
 		if user_answer == self.resistance:
 			print("Correct!")
@@ -31,10 +35,12 @@ class quiz:
 
 
 
-
 def intro():
-	print("Hello students, this program will make you answer questions based on the formula \n"
+	print("Hello students, this program will make you answer questions based on the formula: \n"
 		  "R = (Vs - Vf) / If")
 
+
+read_file = quiz()
 intro()
+
 
